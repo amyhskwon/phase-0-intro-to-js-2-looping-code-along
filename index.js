40698110852ecd206b8934afe927bed591efebb5
amyhskwon <amyhskwon@gmail.com>
2022-11-08 16:50:00 -0800

    commit tree 63ff08ffd2577de6b0d68bacf7c1f317a0a17641

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfNames, event) {
+    const message = [];
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        console.log(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`);
+        message.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+
+function countDown(integer) {
+    while (integer >= 0) {
+        console.log(integer--);
+    }
+}
